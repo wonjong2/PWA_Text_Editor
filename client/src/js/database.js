8c1a3f06f5eba294dc578191842faca60b9d513b
Wonjong Park <wonjong2@gmail.com>
2022-07-08 13:24:30 -0700

    Update getDb function to deliver proper value

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,21 +14,11 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Post to the ase');
-
-  // Create a connection to the database database and version we want to use.
+  console.log('PUT to the database');
   const todosDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
   const tx = todosDb.transaction('jate', 'readwrite');
-
-  // Open up the desired object store.
   const store = tx.objectStore('jate');
-
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({ value: content });
-
-  // Get confirmation of the request.
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 }
@@ -36,24 +26,16 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
-
-  // Create a connection to the database database and version we want to use.
   const todosDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
   const tx = todosDb.transaction('jate', 'readonly');
-
-  // Open up the desired object store.
   const store = tx.objectStore('jate');
-
-  // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
-
-  // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  // console.log('final return value: ', result[0].value)
+  console.log('Result Length:', result.length);
 
+  return result.length ? result[0].value : null;
 }
 
 initdb();
