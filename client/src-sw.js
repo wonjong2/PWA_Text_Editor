d9bd580a28c7174a5354ee274f64d792334f0816
Wonjong Park <wonjong2@gmail.com>
2022-07-10 11:58:11 -0700

    RegisterRoute for asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,5 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-// registerRoute();
+// Implement asset caching
+registerRoute(({ request }) => request.destination === 'image' || 'script' || 'style' || 'manifest', pageCache);
